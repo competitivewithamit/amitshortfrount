ca84392424e4ab9e470570b2cb2d7d5aac9f100e
amit haritwal <65955451+competitivewithamit@users.noreply.github.com>
2020-09-29 11:07:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" render={() => <InsertUrl />} />
-        <Route path="/:id" render={() => <Redirectpage />} />
+        <Route exact path="/:id" component={Redirectpage} />
       </Switch>
     </BrowserRouter>
   );
