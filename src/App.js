7434ade3a0c6c169954f247ea4f43cfdb1dc89da
amit haritwal <65955451+competitivewithamit@users.noreply.github.com>
2020-09-29 11:14:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import InsertUrl from "./InsertUrl";
-import Redirectpage from "./redirectpage";
 
 export default function App() {
   return (
